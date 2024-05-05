83d317f286657fd81fed2740d7720e67a15d915f
CossacksUa <101990398+CossacksUa@users.noreply.github.com>
2024-05-05 21:24:28 +0300

    Добавьте файлы проекта.

---
+++ Lab9/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Lab9
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(417, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(610, 548);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 212);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введіть значення а";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Побудови графіку функції за функцією";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 646);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Lab9/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DrawGraph(double coefficient)
+        {
+            // Очистити малюнок
+            pictureBox1.Image = null;
+            pictureBox1.Refresh();
+
+            // Створити бітмап для малювання
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            // Встановити систему координат
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+
+            // Вісь X
+            g.DrawLine(Pens.Black, 0, centerY, pictureBox1.Width, centerY);
+            for (int x = centerX + 20; x < pictureBox1.Width; x += 20)
+            {
+                g.DrawLine(Pens.Black, x, centerY - 3, x, centerY + 3);
+                g.DrawString((x - centerX) / 20 + "", DefaultFont, Brushes.Black, new PointF(x - 5, centerY + 5));
+            }
+            for (int x = centerX - 20; x > 0; x -= 20)
+            {
+                g.DrawLine(Pens.Black, x, centerY - 3, x, centerY + 3);
+                g.DrawString((x - centerX) / 20 + "", DefaultFont, Brushes.Black, new PointF(x - 5, centerY + 5));
+            }
+
+            // Вісь Y
+            g.DrawLine(Pens.Black, centerX, 0, centerX, pictureBox1.Height);
+            for (int y = centerY + 20; y < pictureBox1.Height; y += 20)
+            {
+                g.DrawLine(Pens.Black, centerX - 3, y, centerX + 3, y);
+                g.DrawString(-(y - centerY) / 20 + "", DefaultFont, Brushes.Black, new PointF(centerX - 20, y - 5));
+            }
+            for (int y = centerY - 20; y > 0; y -= 20)
+            {
+                g.DrawLine(Pens.Black, centerX - 3, y, centerX + 3, y);
+                g.DrawString(-(y - centerY) / 20 + "", DefaultFont, Brushes.Black, new PointF(centerX - 20, y - 5));
+            }
+
+            // Написи на осях
+            g.DrawString("X", DefaultFont, Brushes.Black, new PointF(pictureBox1.Width - 20, centerY - 20));
+            g.DrawString("Y", DefaultFont, Brushes.Black, new PointF(centerX + 10, 0));
+
+            // Малювати графік
+            double scaleX = 20; // Масштаб по X
+            double scaleY = 20; // Масштаб по Y
+            double step = 0.1; // Крок
+            Point prevPoint = new Point();
+            for (double t = 0; t <= 2 * Math.PI; t += step)
+            {
+                double x = coefficient * (t - Math.Sin(t) * Math.Sin(t)) * scaleX;
+                double y = coefficient * (t - Math.Cos(t) * Math.Cos(t)) * scaleY;
+                Point point = new Point((int)(centerX + x), (int)(centerY - y)); // Перевернути Y, тому що ось Y внизу
+                if (t != 0)
+                    g.DrawLine(Pens.Blue, prevPoint, point);
+                prevPoint = point;
+            }
+
+            // Відобразити малюнок на PictureBox
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double coefficient))
+            {
+                DrawGraph(coefficient);
+            }
+            else
+            {
+                MessageBox.Show("Неправильний формат введених даних!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
